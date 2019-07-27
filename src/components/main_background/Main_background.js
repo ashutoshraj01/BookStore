3c270ac7b10fa35751ceebd129e69fa707c59d89
ashutoshraj01 <ashutoshraj908@gmail.com>
2019-07-27 23:38:52 +0530

    BookStore

---
+++ src/components/main_background/Main_background.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./main.css";
+class Main extends React.Component{
+
+    render(){
+        return(
+            <div className="main_background"></div>
+        );
+    }
+
+}
+
+export default Main;
